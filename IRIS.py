7a1e3907f84b259e732fa42473a77ecd5afedc0d
Antonette <amgichohu@gmail.com>
2019-02-10 02:02:27 -0600

    recent

--- IRIS.py
+++ IRIS.py
@@ -5,24 +5,6 @@ from twilio.twiml.messaging_response import Message, MessagingResponse
 from twilio.twiml.messaging_response import Body, Message, Redirect, MessagingResponse
 
 
-app = Flask(__name__)
-
-@app.route("/sms", methods=['GET', 'POST'])
-def sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    # Start our TwiML response
-    resp = MessagingResponse()
-
-    # Add a message
-    resp.message("The Robots are coming! Head for the hills!")
-
-     # Add a picture message
-    #msg.media("https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg")
-
-    return str(resp)
-
-if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0")
 
 message="Hi I'm IRIS, an Immediately Responsive Intelligent System"
 
